819d25187f2af5ec46153c4b0b2ab16a5932b912
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2024-05-15 09:33:24 +0000

    RoseLoverX: auto fixes

--- sticker.py
+++ sticker.py
@@ -1,30 +1,39 @@
 from telethon.tl.functions.messages import GetCustomEmojiDocumentsRequest as get_emojis
 from telethon.tl.functions.messages import GetStickerSetRequest as get_sticker
-from modules._config import bot
 from telethon.tl.types import InputStickerSetShortName
 
-PACK_ID = 'epicmix'
-CHAT = 'rosexchat'
+from modules._config import bot
+
+PACK_ID = "epicmix"
+CHAT = "rosexchat"
+
 
 async def task():
     stickerset = await bot(get_sticker(InputStickerSetShortName(PACK_ID), 0))
     for sticker in stickerset.packs:
         doc = await bot(get_emojis(sticker.documents))
-        
-        dl = await bot.download_media(doc[0], 'stickers')
+
+        await bot.download_media(doc[0], "stickers")
         break
 
+
 def task_A():
-    FILE_NAME = 'stickers.webm'
-    
+    FILE_NAME = "stickers.webm"
+
     # make a 512x512 transparent webm and save as transparent.webm
     import ffmpeg
-    ffmpeg.input('stickers.webm').output('transparent.webm', vf='alphaextract').run(overwrite_output=True)
-    
+
+    ffmpeg.input("stickers.webm").output("transparent.webm", vf="alphaextract").run(
+        overwrite_output=True
+    )
+
     # place FILE_NAME at centre of transparent.webm and save as stickers_e.webm
-    
-    ffmpeg.input('transparent.webm').output('stickers_e.webm', i=FILE_NAME, filter_complex='overlay=(W-w)/2:(H-h)/2').run(overwrite_output=True)
-    
+
+    ffmpeg.input("transparent.webm").output(
+        "stickers_e.webm", i=FILE_NAME, filter_complex="overlay=(W-w)/2:(H-h)/2"
+    ).run(overwrite_output=True)
+
+
 task_A()
 
 # TEST
