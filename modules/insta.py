4766b94ca17ca076bcb320d86e4b2f089e608c02
AmarnathCJD <amarnathcharichilil@gmail.com>
2024-05-26 14:31:42 +0530

    feat: Add Instagram media download functionality

--- modules/insta.py
+++ modules/insta.py
@@ -1,8 +1,94 @@
+from aiohttp import ClientSession, ClientTimeout
 from ._handler import new_cmd
 
 
 @new_cmd(pattern="(insta|instagram|instadl|instadownload)")
 async def _insta(message):
-    # TODO: implement
+    try:
+        url = message.text.split(" ", maxsplit=1)[1]
+    except IndexError:
+        return await message.reply("`Give me a link to download!`")
 
-    await message.reply("This feature is not implemented yet.")
+    resp = await message.reply("`Downloading media...`")
+
+    caption, post_medias, code = await insta_dl(url)
+    if code == -1:
+        return await message.reply(f"**Error:**\n```{post_medias}```")
+
+    if not post_medias:
+        return await message.reply("`No media found!`")
+
+    videos = []
+    images = []
+    stickers = []
+
+    for media in post_medias:
+        if media["type"] == "mp4":
+            videos.append(media["url"])
+        elif media["type"] == "jpg" or media["type"] == "png":
+            images.append(media["url"])
+        elif media["type"] == "webp":
+            stickers.append(media["url"])
+
+    caption_done = False
+
+    if videos:
+        await message.respond(caption, file=videos)
+        caption_done = True
+    if images:
+        if caption_done:
+            await message.respond(file=images)
+        else:
+            await message.respond(caption, file=images)
+            caption_done = True
+    if stickers:
+        await message.respond(file=stickers)
+        if not caption_done:
+            await message.respond(caption)
+
+    await resp.delete()
+
+
+DEFAULT_HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Referer": "https://fastdl.app/en",
+}
+
+
+async def insta_dl(url: str):
+    async with ClientSession(timeout=ClientTimeout(total=10)) as session:
+        async with session.post(
+            "https://fastdl.app/api/convert",
+            json={"url": url},
+            headers=DEFAULT_HEADERS,
+        ) as resp:
+            data = await resp.json()
+            caption = ""
+            post_medias = []
+
+            try:
+                if isinstance(data, dict):
+                    if data.get("meta", {}).get("title"):
+                        caption = data.get("meta", {}).get("title")
+
+                    post_medias.append({
+                        "url": data.get("url", {})[0].get("url"),
+                        "type": data.get("url", {})[0].get("type"),
+                    })
+                elif isinstance(data, list):
+                    for i in data:
+                        if i.get("meta", {}).get("title"):
+                            caption = i.get("meta", {}).get("title")
+
+                        post_medias.append({
+                            "url": i.get("url", {})[0].get("url"),
+                            "type": i.get("url", {})[0].get("type"),
+                        })
+
+                return caption, post_medias, 0
+            except Exception as e:
+                return data, e, -1
+
+
---
+++ webdl.py
@@ -0,0 +1,133 @@
+import os
+from yt_dlp import YoutubeDL
+from subprocess import Popen, PIPE
+import requests
+
+
+def gather_info(id: str):
+    url = f"https://e83f-68-183-237-158.ngrok-free.app/{id}"
+    resp = requests.get(url)
+
+    return resp.json()
+
+
+def download_season():
+    season_id = input("Enter the season ID: ")
+    season_number = input("Enter the season number: ")
+    series_title = input("Enter the series title: ")
+
+    season_number = "0" + \
+        season_number if len(season_number) == 1 else season_number
+    out_file_title = f"{series_title} - S{season_number}E(?).mkv"
+
+    url = f"https://e83f-68-183-237-158.ngrok-free.app/api/episodes?id={
+        season_id}"
+    resp = requests.get(url)
+
+    print("-> downloader - gathering episodes info...")
+
+    ids = [episode["episode_id"] for episode in resp.json()]
+
+    print("-> downloader - found {} episodes...".format(len(ids)))
+    ep_ids = []
+    for i in ids:
+        resp = get_embed_of_episode(i)
+        ep_ids.append({
+            "id": i,
+            "out_file": out_file_title.replace("(?)", "0" + str(ids.index(i) + 1) if len(str(ids.index(i) + 1)) == 1 else str(ids.index(i) + 1)),
+        })
+
+    print("-> downloader - downloading season...")
+    print("-> downloader - this may take a while...")
+
+    i = 0
+    for e in ep_ids:
+        source_hash = requests.get(
+            f"https://e83f-68-183-237-158.ngrok-free.app/api/embed?id={e['id']}&cat=tv").json()["source_hash"]
+
+        print("-> downloader - downloading ({}/{}): {}...".format(i +
+              1, len(ep_ids), e["out_file"]), end="\r")
+        
+        episode_info = gather_info(source_hash)
+        
+        download_url(episode_info["file"], "downloads", e["out_file"])
+        merge_subs(os.path.join("downloads", e["out_file"]), episode_info["subs"])
+        
+        i += 1
+        
+    print("-> downloader - done downloading season...")
+
+
+def get_embed_of_episode(episode_id: str):
+    url = f"https://e83f-68-183-237-158.ngrok-free.app/api/embed?id={
+        episode_id}"
+    resp = requests.get(url)
+
+    return resp.json()
+
+
+def generate_ffmpeg_command(mp4_file_path, subs):
+    ffmpeg_command = ["ffmpeg", "-y", "-i", '"{}"'.format(mp4_file_path)]
+
+    for sub in subs:
+        ffmpeg_command.extend(["-i", sub["file"]])
+
+    output_file_path = mp4_file_path.replace(".mkv", "_subs.mkv")
+
+    ffmpeg_command.extend(["-map", "0:v", "-map", "0:a"])
+
+    for i in range(len(subs)):
+        ffmpeg_command.extend(["-map", f"{i + 1}:s:0"])
+        ffmpeg_command.extend(
+            ["-metadata:s:s:%d" % i, 'language="' + subs[i]["label"] + '"']
+        )
+
+    ffmpeg_command.extend(["-c", "copy", '"{}"'.format(output_file_path)])
+
+    return ffmpeg_command
+
+
+def merge_subs(video_path, subs_paths):
+    ffmpeg_command = generate_ffmpeg_command(video_path, subs_paths)
+
+    print("-> downloader - merging {} subs...".format(len(subs_paths)))
+
+    process = Popen(" ".join(ffmpeg_command),
+                    shell=True, stdout=PIPE, stderr=PIPE)
+    process.communicate()
+
+
+def download_url(url, out_folder, out_filename):
+    ydl_opts = {
+        'quiet': True,
+        'external_downloader': 'aria2c',
+        # 'external_downloader_args': ['-x', '16'],
+        'outtmpl': os.path.join(out_folder, out_filename),
+        'verbose': False,
+        'addmetadata': True,
+        'no-warnings': True,
+    }
+
+    print("-> downloader - downloading video...")
+
+    with YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+
+if __name__ == "__main__":
+    download_season()
+    exec()
+    title_id = input("Enter the ID: ")
+    out_filename = input("Enter the output filename: ")
+    out_folder = "downloads"
+
+    url_info = gather_info(title_id)
+
+    download_url(url_info["file"], out_folder, out_filename)
+    merge_subs(os.path.join(out_folder, out_filename), url_info["subs"])
+
+    print("-> downloader - removing temp files...")
+    os.remove(os.path.join(out_folder, out_filename))
+
+    print("-> downloader - done")
+    print("-> downloader - exiting...")
