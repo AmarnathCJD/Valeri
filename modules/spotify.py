c3a7b867981ec7b8a13a55058bd6791dcf2c10c1
AmarnathCJD <amarnathcharichilil@gmail.com>
2024-07-15 15:08:26 +0530

    ok ok

---
+++ modules/spotify.py
@@ -0,0 +1,21 @@
+from ..spotify.spotify import Spotify
+from ._handler import new_cmd
+
+@new_cmd(pattern='spotify')
+async def spotify(e):
+    s = Spotify()
+    
+    try:
+        track_id = e.text.split(' ')[1]
+    except IndexError:
+        await e.reply('Please provide a track id')
+        return
+    
+    await e.reply('Downloading...')
+    out = s.download(track_id)
+    
+    if out:
+        await e.reply(file=out)
+        
+    else:
+        await e.reply('Failed to download')
