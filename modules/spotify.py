d183ed6fc3855e1e408fba8898d5af39dc7a0ea9
AmarnathCJD <amarnathcharichilil@gmail.com>
2024-07-15 15:35:41 +0530

    ok

--- modules/spotify.py
+++ modules/spotify.py
@@ -1,5 +1,6 @@
 from .spotify_.spotify import Spotify
 from ._handler import new_cmd
+import re
 
 @new_cmd(pattern='spotify')
 async def spotify(e):
@@ -10,6 +11,11 @@ async def spotify(e):
     except IndexError:
         await e.reply('Please provide a track id')
         return
+    
+    if re.match(r"https://open.spotify.com/track/([a-zA-Z0-9]+)", track_id):
+        track_id = re.match(
+        r"https://open.spotify.com/track/([a-zA-Z0-9]+)", track_id).group(1)
+
     
     await e.reply('Downloading...')
     out = s.download(track_id)
